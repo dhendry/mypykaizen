7da6ab81d7bcdb6b6a1e178f2ef95be86629a8b9
GitHub Action <action@github.com>
2020-05-21 04:32:39 +0000

    Bump version: 0.4.0 → 0.5.0-dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mypykaizen",
-    version="0.4.0",
+    version="0.5.0-dev",
     license="MIT",
     packages=find_packages(exclude=["tests"]),
     author="Dan Hendry",
