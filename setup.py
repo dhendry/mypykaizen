5f9e9732f0af63dd6a7fbbbc1266e14b351c0144
GitHub Action <action@github.com>
2020-05-21 05:07:33 +0000

    Bump version: 0.8.0 → 0.9.0-dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mypykaizen",
-    version="0.8.0",
+    version="0.9.0-dev",
     license="MIT",
     packages=find_packages(exclude=["tests"]),
     author="Dan Hendry",
